0650e5016442b095de2e766b9debf617f9fd0e56
Denis Vicente <denis.visente@red-soft.ru>
2025-07-24 17:44:15 +0300

    Add test for create_table RS-193357  - create_private_temporary_table_drop_definition  - create_private_temporary_table_preserve_definition

---
+++ ora2rdb/src/test/resources/scripts/ddl/table/create_table/ptt/create_private_temporary_table_drop_definition_expected.sql
@@ -0,0 +1,6 @@
+/*CREATE [-unconvertible RS-243353 PRIVATE TEMPORARY] TABLE ora$temp_employees (
+    employee_id NUMERIC(34, 8),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    department_id NUMERIC(34, 8)
+) [-unconvertible RS-243353 ON COMMIT DROP DEFINITION];*/
