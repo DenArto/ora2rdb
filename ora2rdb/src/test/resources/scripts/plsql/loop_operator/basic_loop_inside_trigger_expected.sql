68ae280a394089a91a40604a3f2318954abf547f
Denis Vicente <denis.visente@red-soft.ru>
2025-07-25 09:48:49 +0300

    Fix test in loop_operator  - Update expected test outputs to match current implementation

--- ora2rdb/src/test/resources/scripts/plsql/loop_operator/basic_loop_inside_trigger_expected.sql
+++ ora2rdb/src/test/resources/scripts/plsql/loop_operator/basic_loop_inside_trigger_expected.sql
@@ -1,13 +1,16 @@
-CREATE TRIGGER T_Grade_meaning1
-    AFTER INSERT
-    ON EMPLOYEES
-    SQL SECURITY DEFINER
+
+CREATE OR ALTER TRIGGER T_Grade_meaning1
+  AFTER INSERT
+  ON students
+SQL SECURITY DEFINER
 AS
-    DECLARE I NUMERIC(34, 8);
+
+     DECLARE I NUMERIC(34, 8);
 BEGIN
- 	I = 0;
+    I = 0;
     WHILE (TRUE) DO BEGIN
         I = :I + 1;
-        IF( (:I > 5) ) THEN LEAVE;
-    END
-END;
+        IF( (:I > 5) ) THEN 
+        	LEAVE  ;
+    END 
+END ;
