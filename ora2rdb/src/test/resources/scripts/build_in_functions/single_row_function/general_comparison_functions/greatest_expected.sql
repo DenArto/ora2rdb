e643fa8e8198ca84b933a4cec5709e7fea1088c3
Denis Vicente <denis.visente@red-soft.ru>
2025-08-08 18:05:05 +0300

    Fix general_comparison functions tests RS-243795

--- ora2rdb/src/test/resources/scripts/build_in_functions/single_row_function/general_comparison_functions/greatest_expected.sql
+++ ora2rdb/src/test/resources/scripts/build_in_functions/single_row_function/general_comparison_functions/greatest_expected.sql
@@ -1,11 +1,11 @@
-select GREATEST('abs') from rdb$database;
+select GREATEST('abs') from RDB$DATABASE;
 
-select GREATEST('abs', 435) from rdb$database;
+select GREATEST('abs', 435) from RDB$DATABASE;
 
-select GREATEST('abs', 'kfe') from rdb$database;
+select GREATEST('abs', 'kfe') from RDB$DATABASE;
 
-select GREATEST(123, 435) from rdb$database;
+select GREATEST(123, 435) from RDB$DATABASE;
 
-select GREATEST (1, '3.925', '2.4') from rdb$database;
+select GREATEST (1, '3.925', '2.4') from RDB$DATABASE;
 
-select GREATEST('HARRY', 'HARRIOT', 'HAROLD') from rdb$database;
+select GREATEST('HARRY', 'HARRIOT', 'HAROLD') from RDB$DATABASE;
