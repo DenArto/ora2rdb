dc42c8dfe53a391152a0f770397cf4e3b93a78c0
Denis Vicente <denis.visente@red-soft.ru>
2025-08-12 09:51:49 +0300

    Fix test after commenting package RS-194167

--- ora2rdb/src/test/resources/scripts/plsql/exceptions/raise_application_error/raise_application_error_inside_package_body_expected.sql
+++ ora2rdb/src/test/resources/scripts/plsql/exceptions/raise_application_error/raise_application_error_inside_package_body_expected.sql
@@ -1,23 +1,26 @@
+
+
+
 CREATE EXCEPTION PAST_DUE
 	'PAST_DUE EXCEPTION';
 CREATE EXCEPTION CUSTOM_EXCEPTION
 	'error';
 CREATE OR ALTER PACKAGE errnums
-
-SQL SECURITY DEFINER
-AS BEGIN
-    PROCEDURE PP_Grade_meaning1;
+ SQL SECURITY DEFINER
+AS BEGIN 
+    PROCEDURE PP_Grade_meaning1;  
   END;
 
 
-RECREATE PACKAGE BODY errnums
-AS BEGIN
-    --past_due  EXCEPTION;                         -- declare exception
 
+/*RECREATE   PACKAGE BODY errnums
+AS BEGIN 
+    --[-unconvertible RS-245963 past_due  EXCEPTION;]                         -- declare exception
+  
 
     PROCEDURE PP_Grade_meaning1
     AS
     BEGIN
         EXCEPTION CUSTOM_EXCEPTION( 'Account past due.');
-    END
-END;
+    END  
+  END;*/
