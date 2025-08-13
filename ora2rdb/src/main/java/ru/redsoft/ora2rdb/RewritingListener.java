bbd1c2eb230d0d468523a237bfa3271118939330
alexander borisov <aleksandr.borisov@red-soft.ru>
2025-08-13 17:17:59 +0300

    Fix error in test Fix error in test dml/insert/single_table_insert/insert_values_clause

--- ora2rdb/src/main/java/ru/redsoft/ora2rdb/RewritingListener.java
+++ ora2rdb/src/main/java/ru/redsoft/ora2rdb/RewritingListener.java
@@ -3925,21 +3925,21 @@ public class RewritingListener extends PlSqlParserBaseListener {
     @Override
     public void exitInsert_statement(Insert_statementContext ctx) {
 
-
-        Values_clauseContext valuesClause = Finder.getFirstRuleContext(ctx, Values_clauseContext.class);
-        if (valuesClause != null && valuesClause.expressions_() != null) {
-            for (ExpressionContext expression : Finder.getAllRuleContexts(valuesClause, ExpressionContext.class)) {
-                General_element_partContext generalElementPart = Finder.getFirstRuleContext(expression, General_element_partContext.class);
-                if (generalElementPart != null) {
-                    if (generalElementPart.id_expression().size() <= 2
-                            && Ora2rdb.getRealName(generalElementPart.id_expression(generalElementPart.id_expression().size() - 1).getText()).equals("NEXTVAL")) {
-                        insertBefore(expression, "NEXT VALUE FOR ");
-                        delete(generalElementPart.id_expression(1));
-                        delete(generalElementPart.PERIOD(generalElementPart.PERIOD().size() - 1));
-                    }
-                }
-            }
-        }
+//
+//        Values_clauseContext valuesClause = Finder.getFirstRuleContext(ctx, Values_clauseContext.class);
+//        if (valuesClause != null && valuesClause.expressions_() != null) {
+//            for (ExpressionContext expression : Finder.getAllRuleContexts(valuesClause, ExpressionContext.class)) {
+//                General_element_partContext generalElementPart = Finder.getFirstRuleContext(expression, General_element_partContext.class);
+//                if (generalElementPart != null) {
+//                    if (generalElementPart.id_expression().size() <= 2
+//                            && Ora2rdb.getRealName(generalElementPart.id_expression(generalElementPart.id_expression().size() - 1).getText()).equals("NEXTVAL")) {
+//                        insertBefore(expression, "NEXT VALUE FOR ");
+//                        delete(generalElementPart.id_expression(1));
+//                        delete(generalElementPart.PERIOD(generalElementPart.PERIOD().size() - 1));
+//                    }
+//                }
+//            }
+//        }
     }
 
     @Override
