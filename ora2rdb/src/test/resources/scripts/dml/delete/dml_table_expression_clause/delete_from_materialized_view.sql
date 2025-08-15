72725b072dcf246edd30968ae57c289fe140d0c1
Denis Vicente <denis.visente@red-soft.ru>
2025-08-15 18:30:34 +0300

    Fix test delete_from_materialized_view.sql  - remove Cyrillic characters

--- ora2rdb/src/test/resources/scripts/dml/delete/dml_table_expression_clause/delete_from_materialized_view.sql
+++ ora2rdb/src/test/resources/scripts/dml/delete/dml_table_expression_clause/delete_from_materialized_view.sql
@@ -1,8 +1,8 @@
-/*При попытке удалить запись через материализованное представление выдает ошибку
+/*When trying to delete a record through a materialized view, it gives an error
 delete from mv_books;
 Error at Command Line : 6 Column : 13
 Error report -
-SQL Error: ORA-01732: обработка данных для данного предст.пользователя не разрешена
+SQL Error: ORA-01732: data manipulation operation not permitted on this view
 01732. 00000 -  "data manipulation operation not legal on this view"
 *Cause:
 *Action:*/
