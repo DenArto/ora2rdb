7ae1295b7e3fb816bc8a0b01313751023c2d2605
alexander borisov <aleksandr.borisov@red-soft.ru>
2025-08-22 20:00:06 +0300

    Add conversion of conversion functions RS-245838

--- ora2rdb/src/main/java/ru/redsoft/ora2rdb/comments/CommentedListener.java
+++ ora2rdb/src/main/java/ru/redsoft/ora2rdb/comments/CommentedListener.java
@@ -1312,6 +1312,16 @@ public class CommentedListener extends PlSqlParserBaseListener {
                         return;
                 }
         }
+        if (ctx.TO_BINARY_DOUBLE() != null || ctx.TO_BINARY_FLOAT() != null || ctx.TO_DATE() != null
+                || ctx.TO_TIMESTAMP() != null || ctx.TO_TIMESTAMP_TZ() != null)
+            return;
+
+        if (ctx.TO_NUMBER() != null) {
+            if (ctx.format != null)
+                currentBlock.peek().addUnconvertableBlock(ctx.format, Ticket.CONVERT_FUNCTION);
+            return;
+        }
+
         if (ctx.ASCIISTR() != null
                 || ctx.BIN_TO_NUM() != null
                 || ctx.CHARTOROWID() != null
@@ -1320,7 +1330,7 @@ public class CommentedListener extends PlSqlParserBaseListener {
                 || ctx.DECOMPOSE() != null
                 || ctx.NUMTODSINTERVAL() != null
                 || ctx.NUMTOYMINTERVAL() != null
-                || ctx.RAWTONHEX() != null
+                /*|| ctx.RAWTONHEX() != null*/
                 || ctx.ROWIDTOCHAR() != null
                 || ctx.ROWIDTONCHAR() != null
                 || ctx.SCN_TO_TIMESTAMP() != null
@@ -1330,23 +1340,23 @@ public class CommentedListener extends PlSqlParserBaseListener {
                 || ctx.VALIDATE_CONVERSION() != null
                 || ctx.ASCIISTR() != null
                 || ctx.ASCIISTR() != null
-                || ctx.TO_BINARY_DOUBLE() != null
-                || ctx.TO_BINARY_FLOAT() != null
-                || ctx.TO_BLOB() != null
-                || ctx.TO_CHAR() != null
-                || ctx.TO_CLOB() != null
-                || ctx.TO_DATE() != null
+                /*|| ctx.TO_BINARY_DOUBLE() != null*/
+                /*|| ctx.TO_BINARY_FLOAT() != null*/
+                /*|| ctx.TO_BLOB() != null*/
+                /*|| ctx.TO_CHAR() != null*/
+                /*|| ctx.TO_CLOB() != null*/
+                /*|| ctx.TO_DATE() != null*/
                 || ctx.TO_DSINTERVAL() != null
                 || ctx.TO_LOB() != null
                 || ctx.TO_MULTI_BYTE() != null
-                || ctx.TO_NCHAR() != null
-                || ctx.TO_NCLOB() != null
-                || ctx.TO_NUMBER() != null && ctx.format != null
+                /*|| ctx.TO_NCHAR() != null*/
+                /*|| ctx.TO_NCLOB() != null*/
+                /*|| ctx.TO_NUMBER() != null && ctx.format != null*/
                 || ctx.TO_SINGLE_BYTE() != null
-                || ctx.TO_TIMESTAMP() != null
-                || ctx.TO_TIMESTAMP_TZ() != null
+                /*|| ctx.TO_TIMESTAMP() != null*/
+                /*|| ctx.TO_TIMESTAMP_TZ() != null*/
                 || ctx.TO_YMINTERVAL() != null
-                /*|| ctx.CAST() != null*/
+            /*|| ctx.CAST() != null*/
         ) {
             currentBlock.peek().addUnconvertableBlock(ctx, Ticket.CONVERT_FUNCTION);
         }
